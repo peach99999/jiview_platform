af3a1af872eb5bb31cd23fe538b67973ea3a7381
peach <49863818@qq.com>
2019-04-29 13:58:59 +0800

    初始化

---
+++ jiview/src/main/java/com/smaller/jiview/JiviewApplication.java
@@ -0,0 +1,13 @@
+package com.smaller.jiview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiviewApplication.class, args);
+    }
+
+}
