953554e6192b12d6df136c99c30301c79fe3ed6e
peach <49863818@qq.com>
2019-04-30 14:21:04 +0800

    初始化sql

--- jiview/src/main/java/com/smaller/jiview/platform/system/mapper/User=MenuPartMapper.java
+++
@@ -1,8 +0,0 @@
-package com.smaller.jiview.platform.system.mapper;
-
-import com.smaller.jiview.platform.system.model.User=MenuPart;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface User=MenuPartMapper extends Mapper<User=MenuPart>, MySqlMapper<User=MenuPart> {
-}
