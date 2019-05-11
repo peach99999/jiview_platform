764aee92f12a86db12f488b9ba95ebd7366069ed
xiaguifu <xiaguifu@mksoftapp.com>
2019-05-11 17:42:34 +0800

    refs  #13343: 系统code表创建及配置

---
+++ jiview/admin/src/main/java/com/smaller/jiview/admin/platform/system/mapper/SysCodeMapper.java
@@ -0,0 +1,8 @@
+package com.smaller.jiview.admin.platform.system.mapper;
+
+import com.smaller.jiview.admin.platform.system.model.SysCode;
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+public interface SysCodeMapper extends Mapper<SysCode>, MySqlMapper<SysCode> {
+}
